5a43f7629692f910dac7422402aeceb21c260ba4
korut94 <andreamnt94@gmail.com>
2018-04-06 11:20:32 +0200

    Added dummy implementation of the T1-LLI class

---
+++ src/android/T1LLI.java
@@ -0,0 +1,29 @@
+package it.unipi.felicetollot;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContenxt;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class T1LLI extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, CallbackContext);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
